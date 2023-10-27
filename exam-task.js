aaf6437d6ee39d1bd1894910153f4d28c6055fb8
slam814 <abdussalam6111997@gmail.com>
2023-10-27 21:17:07 +0600

    i finesd the work

--- exam-task.js
+++ exam-task.js
@@ -67,3 +67,107 @@ while(numberWhile <= 10){
 for(var i = 0; i <= 10; i++){
     console.log(i, "hellow");
 }
+
+// 6. Declare an array for all the topic that you have learned last few day Diplay then as autput for Loop
+
+var allTheTopics = ["html5", "Css3", "tailwind", "dasiUi", "JavaScript"];
+
+for(var i = 0; i < allTheTopics.length; i++){
+    var myLearnAllTopic = allTheTopics[i];
+    console.log(myLearnAllTopic, i);
+}
+
+
+// 7. Create an array for all the mobile phones. Display all the elements of the array by using a while loop 
+
+var myUseMobilePhoneName =['Semphony c 105', 'wintal w-60', 'Nokia 105', 'Nokia x2', 'Samseng A5', 'SmartPhone'];
+
+var i = 0;
+while(i < myUseMobilePhoneName.length){
+    
+    var usedMobileName = myUseMobilePhoneName[i];
+    console.log(usedMobileName);
+    i++;
+}
+
+// Run a loop from 30 to 86. this Loop will stop if the values get higer then 44
+
+for(var i = 30; i <= 86; i++){
+
+    console.log(i)
+
+    if(i > 44){
+        break;
+    }
+}
+
+// 9. write the price of the books that you have. Display the pricesif the price lower then 200
+
+var booksPrice =[120, 50, 800, 300, 500, 700, 1200, 1500, 100, 5, 20, 115, 739, 99];
+
+for(var i = 0; i < booksPrice.length; i++){
+
+    var bookPriceLeshen200 = booksPrice[i];
+   
+
+    if(bookPriceLeshen200 > 200){
+        continue;
+    }
+
+    console.log(bookPriceLeshen200);
+}
+
+// Variable 
+/*
+1. what JavaScript?
+2. how does Js works?
+3. what is variable?
+4. Declare a variable
+5. Types of variable? How can you find out type of a vara
+6. primitive and non-primitive data types
+7. Naming convention of js variable
+8. Math Operation: +, -, *, /, %
+9. Short hand: +=, -=, /=
+10. ++, --
+11. parseInt, parseFloat
+12. toFixed
+
+
+
+*/
+
+
+/*
+---------------------------------
+                    ARRAY
+--------------------------------
+1. What is the purpose?
+2. how to declare an array in Js 
+3. Number of element in an array
+4. What is index?
+5. find the value of an element by index
+6. change an element by index
+7. get the index of an element by the value
+8. what does it men when you get undefind while getting the value of an element
+9. how can you add an element to an array the last position
+10. how can you remove the last element for array
+11. Add an element at the first position of an array
+12. Remove the first element of an array
+
+
+*/
+
+/*
+-----------------------------------------
+                Conitional
+-----------------------------------------
+
+1. meaning of: >, <, >=, ==, !=, ===
+2. meaning fo: &&
+3. meaning of: ||
+4. let's say you have x amount of money. if you have more then
+if more then 60,000 then will vy gaming Laptop
+if you more then 40,000then you will be Lenovo Yoga
+if you have more hten 20, 000 the will by an uses laptop, otherwise, you will use mobile phone
+
+*/
---
+++ test.js
@@ -0,0 +1,7 @@
+var i = 5;
+
+for (i < 5; i++) {
+
+ console.log(i);
+
+}
